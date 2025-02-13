c08284d2aeb1dfa0f8cde12652b668380a60a257
gardim1 <vini.gardim1@gmail.com>
2025-02-13 17:14:39 -0300

    Primeira vez usando `Spring` e consumindo dados de `API`

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,22 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://omdbapi.com/?t=stranger+things&Season=1&apikey=8b8785d3");
+		System.out.println(json);
+		//json = consumoApi.obterDados("");
+	}
+}
