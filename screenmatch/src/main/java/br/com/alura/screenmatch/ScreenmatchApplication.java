4d342cdc27a1fd0244d5ea8999c89c8964625fce
gardim1 <vini.gardim1@gmail.com>
2025-02-20 18:00:29 -0300

    Modelando episodios

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -23,5 +24,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		json = consumoApi.obterDados("https://omdbapi.com/?t=stranger+things&Season=1&Episode=2&apikey=8b8785d3");
+		DadosEpisodio  dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
 	}
 }
